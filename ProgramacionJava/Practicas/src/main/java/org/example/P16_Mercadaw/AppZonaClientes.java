a8225a716c3caa69b7022c892d3ac64f398ff57d
MANUELA\manue <manuelaplanelles@gmail.com>
2026-03-25 20:26:49 +0100

    se termina la clase mercadaw, y se realiza el metodo autenticación y el main, intentamos probar, pero salta error de la p15, queda pendiente solucionar la practica

--- ProgramacionJava/Practicas/src/main/java/org/example/P16_Mercadaw/AppZonaClientes.java
+++ ProgramacionJava/Practicas/src/main/java/org/example/P16_Mercadaw/AppZonaClientes.java
@@ -1,5 +1,6 @@
 package org.example.P16_Mercadaw;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class AppZonaClientes {
@@ -8,18 +9,35 @@ public class AppZonaClientes {
     private static Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Mercadaw.generarClientes();
+        autenticacion(Mercadaw.getClientes());
+    }
+    static public void autenticacion(List<Cliente> clientes){
+        System.out.println("*** COMPRA ONLINE DE MERCADAW ***");
+        for (int intentos = 0; intentos < 3; intentos++) {
+
+            System.out.println("Usuario:");
+            String usuarioIntroducido = teclado.next();
+            System.out.println("Contraseña:");
+            String contrasenyaIntroducido = teclado.next();
+
+            for (Cliente cliente : clientes) {
+                if (cliente.getUsuario().equals(usuarioIntroducido) && cliente.getContrasenya().equals(contrasenyaIntroducido)) {
+                    System.out.println("Usuaria y contraseña correctas.");
+                    iniciarCompra();
+                }
+            }
+            System.out.println("Algo no coincide o no existe! Vuelve a intentarlo...");
+        }
+        System.out.println("ERROR DE AUTENTICACIÓN.");
+    }
+    static public void iniciarCompra(){
 
     }
-    public void autentificacion(){
+    static public void imprimirProductos(){
 
     }
-    public void iniciarCompra(){
-
-    }
-    public void imprimirProductos(){
-
-    }
-    public void imprimirDespedida(){
+    static public void imprimirDespedida(){
 
     }
 }
--- ProgramacionJava/Practicas/src/main/java/org/example/P16_Mercadaw/Mercadaw.java
+++ ProgramacionJava/Practicas/src/main/java/org/example/P16_Mercadaw/Mercadaw.java
@@ -1,28 +1,28 @@
 package org.example.P16_Mercadaw;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+
 public class Mercadaw {
 
     private static List<Cliente> clientes = new ArrayList<>();
 
     public Mercadaw() {
-        generarClientes();
     }
 
     public static void generarClientes() {
-        /*String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        String caracter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         Random random = new Random();
-        for (int i = 0; i < 3; i++) {
-            StringBuilder usuario = new StringBuilder();
-            StringBuilder contrasenya = new StringBuilder();
-            for (int j = 0; j < 8; j++) {
-                usuario.append(caracteres.charAt(random.nextInt(caracteres.length())));
-                contrasenya.append(caracteres.charAt(random.nextInt(caracteres.length())));
-            }
-            clientes.add(new Cliente(usuario.toString(),contrasenya.toString()));
-        }*/
+        String contructorUsuario = "";
+        String contructorContrasenya = "";
+        for(int i = 0; i<8; i++){
+            contructorContrasenya += caracter.charAt(random.nextInt(0,62));
+            contructorUsuario += caracter.charAt(random.nextInt(0,62));
+        }
+        clientes.add(new Cliente(contructorUsuario, contructorContrasenya));
+        System.out.println("Usuario creado: " + contructorUsuario);
+        System.out.println("Contraseña creada: " + contructorContrasenya);
     }
 
     public static List<Cliente> getClientes() {
