5f54d380335f3feee1e44a4422f068b8c15a3b61
MANUELA\manue <manuelaplanelles@gmail.com>
2026-03-26 15:00:05 +0100

    testing

--- ProgramacionJava/Practicas/src/main/java/org/example/P5_NumeroDeLaSuerte/numeroDeLASuerte.java
+++ ProgramacionJava/Practicas/src/main/java/org/example/P5_NumeroDeLaSuerte/numeroDeLASuerte.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class numeroDeLASuerte {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-
         System.out.println("Introduce tu fecha de nacimiento (dd/mm/aaaa)");
         String fecha = teclado.next();
-
         int primeraBarra = fecha.indexOf("/");
         int segundaBarra = fecha.lastIndexOf("/");
 
@@ -18,7 +16,6 @@ public class numeroDeLASuerte {
         } else {
             System.out.println("Formato válida, procesando...");
         }
-
         String diaTexto = fecha.substring(0, primeraBarra);
         String mesTexto = fecha.substring(primeraBarra + 1, segundaBarra);
         String anoTexto = fecha.substring(segundaBarra + 1);
@@ -27,7 +24,6 @@ public class numeroDeLASuerte {
             System.out.println("ERROR: el año debe tener 4 cifras.");
             return;
         }
-
         int dia = Integer.parseInt(diaTexto);
         int mes = Integer.parseInt(mesTexto);
         int ano = Integer.parseInt(anoTexto);
@@ -40,7 +36,6 @@ public class numeroDeLASuerte {
             System.out.println("ERROR: el mes debe estar entre 1 y 12.");
             return;
         }
-
         int subtotal = dia + mes + ano;
         System.out.println(dia + " + " + mes + " + " + ano + " = " + subtotal);
 
