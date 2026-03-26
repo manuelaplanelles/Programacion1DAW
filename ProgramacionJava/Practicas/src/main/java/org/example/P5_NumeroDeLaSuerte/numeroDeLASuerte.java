15e281da8ef17a2fff026e638fb5f46f90184113
MANUELA\manue <manuelaplanelles@gmail.com>
2026-03-26 13:13:39 +0100

    se detecta en las pruebas que hace siempre substring(0,2) y substring(2), es decir, parte el subtotal fijo en los primeros 2 dígitos y el resto. Pero el ejemplo del enunciado hace 1+9+9+9 (cifra a cifra), no 19+99. se corrige este apartado.

--- ProgramacionJava/Practicas/src/main/java/org/example/P5_NumeroDeLaSuerte/numeroDeLASuerte.java
+++ ProgramacionJava/Practicas/src/main/java/org/example/P5_NumeroDeLaSuerte/numeroDeLASuerte.java
@@ -15,7 +15,7 @@ public class numeroDeLASuerte {
         if (!fecha.contains("/") || primeraBarra == segundaBarra || fecha.contains("-")) {
             System.out.println("ERROR: fecha no válida, usa el formato dd/mm/aaaa con números positivos.");
             return;
-        }else{
+        } else {
             System.out.println("Fecha válida, procesando...");
         }
 
@@ -23,24 +23,28 @@ public class numeroDeLASuerte {
         String mesTexto = fecha.substring(primeraBarra + 1, segundaBarra);
         String anoTexto = fecha.substring(segundaBarra + 1);
 
-
         int dia = Integer.parseInt(diaTexto);
         int mes = Integer.parseInt(mesTexto);
         int ano = Integer.parseInt(anoTexto);
 
-        int subtotal = dia+mes+ano;
-
-        System.out.println(dia+ " + " + mes + " + " + ano + " = " + subtotal);
+        int subtotal = dia + mes + ano;
+        System.out.println(dia + " + " + mes + " + " + ano + " = " + subtotal);
 
         String subtotalTexto = String.valueOf(subtotal);
-        String subtotalTex1 = subtotalTexto.substring(0, 2);
-        String subtotalTex2 = subtotalTexto.substring(2);
+        int total = 0;
+        String calculo = "";
 
-        int subtotal1 = Integer.parseInt(subtotalTex1);
-        int subtotal2 = Integer.parseInt(subtotalTex2);
-        int total =  subtotal1+subtotal2;
-        System.out.println(subtotal1 + " + " + subtotal2 + " = " + total);
+        for (int i = 0; i < subtotalTexto.length(); i++) {
+            int cifra = Integer.parseInt(subtotalTexto.substring(i, i + 1));
+            total += cifra;
+            if (i < subtotalTexto.length() - 1) {
+                calculo += cifra + "+";
+            } else {
+                calculo += cifra;
+            }
+        }
+
+        System.out.println(calculo + " = " + total);
         System.out.println("Tu número de la suerte es el: " + total);
     }
 }
-
