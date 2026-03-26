ea7190c0ef30ab6a6a6e2cdbd4b7fddf273aac5b
MANUELA\manue <manuelaplanelles@gmail.com>
2026-03-26 14:11:44 +0100

    haciendo las pruebas se analiza que el formato lo identifica correctamente pero no hay ningun control sobre los numeros introducido, para solucionarlo se implemte un if que controle dia del 1 al 31 y mes del 1 al 12

--- ProgramacionJava/Practicas/src/main/java/org/example/P5_NumeroDeLaSuerte/numeroDeLASuerte.java
+++ ProgramacionJava/Practicas/src/main/java/org/example/P5_NumeroDeLaSuerte/numeroDeLASuerte.java
@@ -16,35 +16,44 @@ public class numeroDeLASuerte {
             System.out.println("ERROR: fecha no válida, usa el formato dd/mm/aaaa con números positivos.");
             return;
         } else {
-            System.out.println("Fecha válida, procesando...");
+            System.out.println("Formato válida, procesando...");
         }
 
         String diaTexto = fecha.substring(0, primeraBarra);
         String mesTexto = fecha.substring(primeraBarra + 1, segundaBarra);
         String anoTexto = fecha.substring(segundaBarra + 1);
 
+        if (anoTexto.length() != 4) {
+            System.out.println("ERROR: el año debe tener 4 cifras.");
+            return;
+        }
+
         int dia = Integer.parseInt(diaTexto);
         int mes = Integer.parseInt(mesTexto);
         int ano = Integer.parseInt(anoTexto);
+
+        if (dia < 1 || dia > 31) {
+            System.out.println("ERROR: el día debe estar entre 1 y 31.");
+            return;
+        }
+        if (mes < 1 || mes > 12) {
+            System.out.println("ERROR: el mes debe estar entre 1 y 12.");
+            return;
+        }
 
         int subtotal = dia + mes + ano;
         System.out.println(dia + " + " + mes + " + " + ano + " = " + subtotal);
 
         String subtotalTexto = String.valueOf(subtotal);
-        int total = 0;
-        String calculo = "";
 
-        for (int i = 0; i < subtotalTexto.length(); i++) {
-            int cifra = Integer.parseInt(subtotalTexto.substring(i, i + 1));
-            total += cifra;
-            if (i < subtotalTexto.length() - 1) {
-                calculo += cifra + "+";
-            } else {
-                calculo += cifra;
-            }
-        }
+        int cifra1 = Integer.parseInt(subtotalTexto.substring(0, 1));
+        int cifra2 = Integer.parseInt(subtotalTexto.substring(1, 2));
+        int cifra3 = Integer.parseInt(subtotalTexto.substring(2, 3));
+        int cifra4 = Integer.parseInt(subtotalTexto.substring(3, 4));
 
-        System.out.println(calculo + " = " + total);
+        int total = cifra1 + cifra2 + cifra3 + cifra4;
+
+        System.out.println(cifra1 + "+" + cifra2 + "+" + cifra3 + "+" + cifra4 + " = " + total);
         System.out.println("Tu número de la suerte es el: " + total);
     }
 }
