569e15d21ce2c78d45c2c4163608be5e2d58c967
MANUELA\manue <manuelaplanelles@gmail.com>
2026-03-30 23:08:38 +0200

    retocamos herrores descubiertos en entorno de prueba , y dejamos informe finalizada

--- ProgramacionJava/Practicas/src/main/java/org/example/P6_CalculadoraSimple/calculadoraSimple.java
+++ ProgramacionJava/Practicas/src/main/java/org/example/P6_CalculadoraSimple/calculadoraSimple.java
@@ -1,14 +1,18 @@
 package org.example.P6_CalculadoraSimple;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class calculadoraSimple {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        String operacion ;
-        System.out.println("*** BIENVENIDO A LA CALCULADORA RAPIDA ***");
+        String operacion = "";
+        
+        System.out.println("*** BIENVENIDO A LA CALCULADORA RÁPIDA ***");
+        
         do{
-            System.out.println("Introduce el operador: ");
+            try{    
+                System.out.println("Introduce el operador: ");
             int operador1 = teclado.nextInt();
             System.out.println("----------------------------");
             System.out.println("[+] -> sumar");
@@ -21,37 +25,35 @@ public class calculadoraSimple {
             System.out.println("Introduce el operador: ");
             operacion = teclado.next().toUpperCase();
 
-            int operador2 = 0;
-            if(!operacion.equals("R") && !operacion.equals("SALIR")){
-                System.out.println("Introduce el segundo operador: ");
-                operador2= teclado.nextInt();
-            }
-
             switch (operacion){
                 case "+":
-                    int suma = operador1+operador2;
-                    System.out.println("El resultado de "+operador1+" + "+operador2+" es "+suma);
+                    System.out.println("Introduce el segundo operando: ");
+                    int operador2 = teclado.nextInt();
+                    System.out.println("El resultado de " + operador1 + " + " + operador2 + " es " + (operador1 + operador2));
                     break;
                 case "-":
-                    int resta = operador1-operador2;
-                    System.out.println("El resultado de "+operador1+" - "+operador2+" es "+resta);
+                    System.out.println("Introduce el segundo operando: ");
+                    int operador3 = teclado.nextInt();
+                    System.out.println("El resultado de " + operador1 + " - " + operador3 + " es " + (operador1 - operador3));
                     break;
                 case "X":
-                    int multi = operador1*operador2;
-                    System.out.println("El resultado de "+operador1+" x "+operador2+" es "+multi);
+                    System.out.println("Introduce el segundo operando: ");
+                    int operador4 = teclado.nextInt();
+                    System.out.println("El resultado de " + operador1 + " x " + operador4 + " es " + (operador1 * operador4));
                     break;
                 case "/":
-
-                    if (operador2==0) {
+                    System.out.println("Introduce el segundo operando: ");
+                    int operador5 = teclado.nextInt();
+                    if (operador5 == 0) {
                         System.out.println("ERROR, no se puede dividir entre 0");
-                    }else {
-                        int divi = operador1 / operador2;
-                        System.out.println("El resultado de " + operador1 + " / " + operador2 + " es " + divi);
+                    } else {
+                        double divi = (double) operador1 / operador5;
+                        System.out.println("El resultado de " + operador1 + " / " + operador5 + " es " + divi);
                     }
                     break;
                 case "R":
                     double resul = Math.sqrt(operador1);
-                    System.out.println("El resultado de la raiz cuadrada de " + operador1 + " es " + resul);
+                    System.out.println("La raíz cuadrada de " + operador1 + " es " + resul);
                     break;
                 case "SALIR":
                     System.out.println("¡Hasta luego!");
@@ -60,6 +62,11 @@ public class calculadoraSimple {
                     System.out.println("ERROR, la operación no es valido");
                     break;
             }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes introducir un número.");
+                teclado.next();
+            }
         }while (!operacion.equals("SALIR"));
+        
     }
 }
