35b63bf30dc773471f680be1eb5142c05e284c6d
MANUELA\manue <manuelaplanelles@gmail.com>
2026-04-01 00:11:32 +0200

    se arreglan unos bugs del sorteo

--- ProgramacionJava/Practicas/src/main/java/org/example/P9_SimulacionSorteo/prueba.java
+++ ProgramacionJava/Practicas/src/main/java/org/example/P9_SimulacionSorteo/prueba.java
@@ -4,11 +4,51 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class simulacionSorteo {
+public class prueba {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         Random random = new Random();
 
+        // SORTEO
+        int[] sorteo = new int[6];
+        for (int i = 0; i < 6; i++) {
+            int numero;
+            boolean repetido;
+            do {
+                numero = random.nextInt(49) + 1;
+                repetido = false;
+                for (int j = 0; j < i; j++) {
+                    if (sorteo[j] == numero) {
+                        repetido = true;
+                    }
+                }
+            } while (repetido);
+            sorteo[i] = numero;
+        }
+        Arrays.sort(sorteo);
+
+        // COMPLEMENTARIO
+        int complementario;
+        boolean repetido;
+        do {
+            complementario = random.nextInt(49) + 1;
+            repetido = false;
+            for (int i = 0; i < 6; i++) {
+                if (sorteo[i] == complementario) {
+                    repetido = true;
+                }
+            }
+        } while (repetido);
+
+        // REINTEGRO
+        int reintegroSorteo = random.nextInt(10);
+
+        // MOSTRAR SORTEO (solo para pruebas, quitar después)
+        System.out.println("Números sorteados: " + Arrays.toString(sorteo));
+        System.out.println("Complementario: " + complementario);
+        System.out.println("Reintegro: " + reintegroSorteo);
+
+        // BOLETO
         System.out.println("Introduce los datos de tu boleto: ");
         String boleto = teclado.next();
 
@@ -26,43 +66,7 @@ public class simulacionSorteo {
 
         System.out.println(Arrays.toString(datosBoleto));
 
-        int[] sorteo = new int[6];
-        for (int i = 0; i < 6; i++) {
-            int numero;
-            boolean repetido;
-            do {
-                numero = random.nextInt(49) + 1;
-                repetido = false;
-                for (int j = 0; j < i; j++) {
-                    if (sorteo[j] == numero) {
-                        repetido = true;
-                    }
-                }
-            } while (repetido);
-            sorteo[i] = numero;
-            System.out.println(Arrays.toString(sorteo));
-        }
-        Arrays.sort(sorteo);
-
-        int complementario;
-        boolean repetido;
-        do {
-            complementario = random.nextInt(49) + 1;
-            repetido = false;
-            for (int i = 0; i < 6; i++) {
-                if (sorteo[i] == complementario) {
-                    repetido = true;
-                }
-            }
-        } while (repetido);
-
-        int reintegroSorteo = random.nextInt(10);
-
-        System.out.println("SORTEO:");
-        System.out.println(Arrays.toString(sorteo));
-        System.out.println("Complementario: " + complementario);
-        System.out.println("Reintegro: " + reintegroSorteo);
-
+        // COMPROBAR ACIERTOS
         int aciertos = 0;
         boolean tieneComplementario = false;
 
@@ -79,6 +83,7 @@ public class simulacionSorteo {
 
         boolean tieneReintegro = (reintegroBoleto == reintegroSorteo);
 
+        // RESULTADOS
         System.out.println("RESULTADOS:");
         if (aciertos == 6 && tieneReintegro) {
             System.out.println("Categoría Especial.");
