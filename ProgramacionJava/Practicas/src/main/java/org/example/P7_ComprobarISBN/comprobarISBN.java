6e8cbd6f71d99b9d6a7351496ad4894abb7d09e6
MANUELA\manue <manuelaplanelles@gmail.com>
2026-03-31 22:26:24 +0200

    corrijo bugs en las pruebas

--- ProgramacionJava/Practicas/src/main/java/org/example/P7_ComprobarISBN/comprobarISBN.java
+++ ProgramacionJava/Practicas/src/main/java/org/example/P7_ComprobarISBN/comprobarISBN.java
@@ -8,7 +8,7 @@ public class comprobarISBN {
         System.out.println("Introduzca ISBN: ");
         String isbn = teclado.next().toUpperCase();
 
-        if(isbn.length() !=10){
+        if (isbn.length() != 10) {
             System.out.println("ERROR: el ISBN debe tener exactamente 10 caracteres");
             return;
         }
@@ -17,11 +17,10 @@ public class comprobarISBN {
         } else {
             comprobarISBN(isbn);
         }
-
     }
-    public static void comprobarISBN(String isbn){
-        int suma = 0;
 
+    public static int calcularSuma(String isbn) {
+        int suma = 0;
         for (int i = 0; i < isbn.length(); i++) {
             int multiplicador = 10 - i;
             int cifra;
@@ -32,51 +31,30 @@ public class comprobarISBN {
             }
             suma += cifra * multiplicador;
         }
+        return suma;
+    }
 
-        if (suma % 11 == 0) {
+    public static void comprobarISBN(String isbn) {
+        if (calcularSuma(isbn) % 11 == 0) {
             System.out.println("El ISBN es válido.");
         } else {
             System.out.println("El ISBN no es válido.");
         }
     }
 
-    public static void repararISBN(String isbn){
+    public static void repararISBN(String isbn) {
         int posicion = isbn.indexOf("?");
 
         for (int intento = 0; intento <= 9; intento++) {
             String isbnReparado = isbn.substring(0, posicion) + intento + isbn.substring(posicion + 1);
-
-            int suma = 0;
-            for (int i = 0; i < isbnReparado.length(); i++) {
-                int multiplicador = 10 - i;
-                int cifra;
-                if (isbnReparado.charAt(i) == 'X') {
-                    cifra = 10;
-                } else {
-                    cifra = isbnReparado.charAt(i) - '0';
-                }
-                suma += cifra * multiplicador;
-            }
-
-            if (suma % 11 == 0) {
+            if (calcularSuma(isbnReparado) % 11 == 0) {
                 System.out.println("El dígito que falta es " + intento);
                 return;
             }
         }
 
-        String isbnReparado = isbn.substring(0, posicion) + "X" + isbn.substring(posicion + 1);
-        int suma = 0;
-        for (int i = 0; i < isbnReparado.length(); i++) {
-            int multiplicador = 10 - i;
-            int cifra;
-            if (isbnReparado.charAt(i) == 'X') {
-                cifra = 10;
-            } else {
-                cifra = isbnReparado.charAt(i) - '0';
-            }
-            suma += cifra * multiplicador;
-        }
-        if (suma % 11 == 0) {
+        String isbnReparadoX = isbn.substring(0, posicion) + "X" + isbn.substring(posicion + 1);
+        if (calcularSuma(isbnReparadoX) % 11 == 0) {
             System.out.println("El dígito que falta es X");
         } else {
             System.out.println("No se encontró ningún dígito válido.");
